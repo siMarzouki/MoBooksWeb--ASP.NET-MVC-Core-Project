1b67f418da454788571198b09cd4df275717c5a0
moazmarES <81397619+moazmar@users.noreply.github.com>
2023-03-20 02:04:02 +0100

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoBooksWeb.Data;
+using MoBooksWeb.Models;
+
+namespace MoBooksWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDBContect _db;
+
+        public CategoryController(ApplicationDBContect db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Data/ApplicationDBContect.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoBooksWeb.Models;
+
+namespace MoBooksWeb.Data
+{
+    public class ApplicationDBContect:DbContext
+    {
+        public ApplicationDBContect(DbContextOptions<ApplicationDBContect> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
